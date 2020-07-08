86c132c804e141911bca3738a187ccc822a272ba
bradleyloh <bradleylohwenhui@gmail.com>
2020-07-09 01:09:41 +0800

    Add project files.

---
+++ Continental1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Continental1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_extract = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Btn_extract
+            // 
+            this.Btn_extract.Location = new System.Drawing.Point(362, 59);
+            this.Btn_extract.Name = "Btn_extract";
+            this.Btn_extract.Size = new System.Drawing.Size(75, 23);
+            this.Btn_extract.TabIndex = 0;
+            this.Btn_extract.Text = "Extract";
+            this.Btn_extract.UseVisualStyleBackColor = true;
+            this.Btn_extract.Click += new System.EventHandler(this.btn_extract_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(48, 103);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(740, 313);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Btn_extract);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_extract;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Continental1/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FireSharp.Config;         //Usage of FireSharp Plugins to connect to Firebase
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Tulpep.NotificationWindow;     //For windows notification popup
+
+namespace Continental1
+{
+    public partial class Form1 : Form
+    {
+
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "VAZ9i5Nhzn8elUoVOHXi9VDaeyVLlXLIRpxmP8QN",             //Connection requirements to firebase
+            BasePath = "https://continental1.firebaseio.com/"
+        };
+        IFirebaseClient client;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new FireSharp.FirebaseClient(config);
+
+            if (client != null)
+            {
+                MessageBox.Show("Database connected");
+            }
+            dt.Columns.Add("PID");                           //For DataGridView display
+            dt.Columns.Add("bn");
+            dt.Columns.Add("bpm");
+            dt.Columns.Add("cmd");
+            dt.Columns.Add("pip");
+            dt.Columns.Add("spo2");
+
+
+            dataGridView1.DataSource = dt;
+        }
+
+        private async void btn_extract_Click(object sender, EventArgs e)
+        {
+            int i = 1;
+            dt.Rows.Clear();        //Clear before extracting to prevent re-read
+            while (true)            //To loop infinite
+            {
+
+                try
+                {
+                    FirebaseResponse resp2 = await client.GetTaskAsync("Patient" + i);          //Extracting command from Firebase
+                    Data obj2 = resp2.ResultAs<Data>();
+
+
+                    DataRow row = dt.NewRow();         //Initialising the Data into DataGridView
+
+                    row["PID"] = obj2.PID;
+                    row["bn"] = obj2.bn;
+                    row["bpm"] = obj2.bpm;
+                    row["cmd"] = obj2.cmd;
+                    row["pip"] = obj2.pip;
+                    row["spo2"] = obj2.spo2;
+
+                    dt.Rows.Add(row);                 //Execution of Data into DataGridView
+
+
+                    
+                    var popupNotifier = new PopupNotifier();                               //Popup notification 
+                    popupNotifier.TitleText = "Patient PID" + obj2.PID;
+                    popupNotifier.ContentText = "Issues" + obj2.bn+ '\n' + obj2.bpm + '\n' + obj2.cmd+ '\n' + obj2.pip + '\n' + obj2.spo2;
+                    popupNotifier.IsRightToLeft = false;
+                    popupNotifier.Popup();
+
+
+
+                    i++;
+
+                }
+                catch                                //If error occurs
+                {
+                    await Task.Delay(2000);          //Delay 2seconds 
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        DataTable dt = new DataTable();
+
+        
+    }
+
+    internal class Data             //Class declare to store data into variable
+    {
+        public string PID{ get; set; }
+        public string bn { get; set; }
+        public string bpm { get; set; }
+        public string cmd { get; set; }
+        public string pip{get; set; }
+        public string spo2 { get; set; }
+    }
+}
